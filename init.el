278a401dd8efbd27f676f520290cc0c60b0ae2fc
Lee Johnson <lee.james.johnson@gmail.com>
2022-06-17 12:26:49 -0400

    develop - 	modified:   init.el

--- init.el
+++ init.el
@@ -29,6 +29,12 @@
 ;;        (list (expand-file-name "~/.demacs.d/lisp/"))
 ;;        original-load-path))
 
+(let ((minver "25.1"))
+  (when (version< emacs-version minver)
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+(when (version< emacs-version "26.1")
+  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'init-benchmarking) ;; Measure startup time
 
@@ -249,3 +255,4 @@
 	  (desktop-save desktop-dirname)))
 
 (add-hook 'kill-emacs-hook 'my-desktop-save)
+(add-hook 'kill-emacs-hook 'desktop-release-lock)
